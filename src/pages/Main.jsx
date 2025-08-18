e06c657ba1ec4087b6c590c8266068b0f3b64134
croissen <croissen214@gmail.com>
2025-08-19 00:35:26 +0900

    First commit: SeoulEazy 프로젝트 생성

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Main.styles';
+
+function Main() {
+  return (
+    <S.Container>
+      <S.Title>어디긴? SeoulEazy!!</S.Title>
+    </S.Container>
+  );
+}
+
+export default Main;
---
+++ src/pages/Main.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;       /* 화면 전체 높이 */
+  background-color: #f5f5f5;
+`;
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  color: #333;
+  font-weight: bold;
+`;
